30b5d45952248c89495749a50c512f101aee6308
Mark D'Arensbourg <Mark.DArensbourg@archoninfosys.com>
2014-06-06 12:02:41 -0500

    Create a miraculous world of auto-sizing textareas

---
+++ README.md
@@ -0,0 +1,19 @@
+#Knockout Bootstrap Modal Binding
+
+> A simple knockout binding for textareas which automagically adjust their height based on content.
+
+##Install with [Bower](http://bower.io/)
+
+```
+bower install knockout-magic-textarea
+```
+
+Then add `knockout.magic.textarea.js` to your project.
+
+##How to Use
+
+Include the script on your page (either via a normal script tag or via an AMD loader). Then bind it to an element:
+
+```html
+<textarea data-bind="magicTextarea: true"></textarea>
+```
---
+++ knockout.magic.textarea.js
@@ -0,0 +1,20 @@
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(["knockout", "jquery"], factory);
+	} else {
+		// Browser globals
+		factory(ko, $);
+	}
+}(this, function(ko, $) {
+	ko.bindingHandlers.magicTextarea = {
+		init: function(element) {
+			var $element = $(element);
+
+			$element.on('keydown keyup input paste propertychange click submit mouseenter', function(){
+				element.style.height = "auto";
+				$element.height(element.scrollHeight - element.clientHeight);
+			});
+		}
+	};
+}));
