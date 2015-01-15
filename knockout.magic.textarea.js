16955415c5b133914ff0a78b0b32a72d4d00cb66
Mark D'Arensbourg <Mark.DArensbourg@archoninfosys.com>
2015-01-15 17:24:56 -0600

    Attempt to calculate layout immediately to avoid jump on interaction.
    Still doesn't solve the case of when the element doesn't know it's not visible yet.

--- knockout.magic.textarea.js
+++ knockout.magic.textarea.js
@@ -1,4 +1,4 @@
-(function(root, factory) {
+(function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define(["knockout", "jquery"], factory);
@@ -6,16 +6,30 @@
 		// Browser globals
 		factory(ko, $);
 	}
-}(this, function(ko, $) {
-	ko.bindingHandlers.magicTextarea = {
-		init: function(element) {
-			var $element = $(element);
+}(this, function (ko, $) {
+	var MIN_HEIGHT = 32; // 2 lines for body copy
 
-			$element.on('keydown keyup input paste propertychange click submit mouseenter', function(){
-				element.style.height = "auto";
-				var newHeight = element.scrollHeight + parseInt(window.getComputedStyle(element)["padding-top"]) + parseInt(window.getComputedStyle(element)["padding-bottom"]);
-				$element.outerHeight(newHeight);
+	function calculateLayout(element) {
+		element.style.height = "auto";
+		var newHeight = Math.max(MIN_HEIGHT, element.scrollHeight) + parseInt(window.getComputedStyle(element)["padding-top"]) + parseInt(window.getComputedStyle(element)["padding-bottom"]);
+		$(element).outerHeight(newHeight);
+	}
+
+	ko.bindingHandlers.magicTextarea = {
+		init: function (element) {
+			setTimeout(function(){
+				calculateLayout(element);
+			}, 0);
+			$(element).on('keydown keyup input paste propertychange click submit mouseenter', function() {
+				setTimeout(function(){
+					calculateLayout(element);
+				}, 0);
 			});
+		},
+		update: function(element) {
+				setTimeout(function(){
+					calculateLayout(element);
+				}, 0);
 		}
 	};
 }));
